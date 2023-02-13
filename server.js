9522d2999149e4748b316034c687f25373e95008
Dontani Fellini <pinkdneal@gmail.com>
2023-02-13 08:16:38 -0500

    patient route changed to patients

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/patient",patientRoutes);
+app.use("/patients",patientRoutes);
 app.use("/comment", commentRoutes);
 
 //Server Running
